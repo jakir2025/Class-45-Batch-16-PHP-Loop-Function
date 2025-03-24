81b7213c9bccf772a67495cb2f7ed2ca4e01763e
jakir2025 <jakirhusen5002@gmail.com>
2025-03-24 19:40:27 +0600

    PHP,Function

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP-45</title>
+</head>
+<body>
+  
+<?php
+for($i=0; $i<=5; $i++){
+    echo  "Hellow World!.<br>";
+  }
+
+
+
+  $x = 10;
+  $y = 20;
+
+  $result = $x+$y;
+
+  echo $result;
+?>
+
+</body>
+</html>
